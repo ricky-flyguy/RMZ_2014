2c1b470295d759876dbb46c0a15795eaeddaf500
Ricardo Hyde <ricardohyde15@gmail.com>
2014-05-11 00:55:29 -0400

    no message

---
+++ Game2014/proj.android/bin/classes/R.java.d
@@ -0,0 +1,6 @@
+C:\Users\Ricardo\Desktop\RMZREPO\Game2014\proj.android\gen\rmz\appz\cocos2Dx\R.java \
+ : C:\Users\Ricardo\Desktop\RMZREPO\Game2014\proj.android\res\drawable-hdpi\icon.png \
+C:\Users\Ricardo\Desktop\RMZREPO\Game2014\proj.android\res\drawable-ldpi\icon.png \
+C:\Users\Ricardo\Desktop\RMZREPO\Game2014\proj.android\res\drawable-mdpi\icon.png \
+C:\Users\Ricardo\Desktop\RMZREPO\Game2014\proj.android\res\values\strings.xml \
+C:\Users\Ricardo\Desktop\RMZREPO\Game2014\proj.android\bin\AndroidManifest.xml \
