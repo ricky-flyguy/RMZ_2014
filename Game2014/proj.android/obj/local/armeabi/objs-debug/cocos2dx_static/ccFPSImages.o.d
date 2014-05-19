abe8494e915bdb643f6c83ecc9056c4a79b53b53
Zachary Warner <zbwarner@gmail.com>
2014-05-18 21:49:50 -0400

    gitignore change - Z-Plane

--- Game2014/proj.android/obj/local/armeabi/objs-debug/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs-debug/cocos2dx_static/ccFPSImages.o: \
- C:\Users\Ricardo\Desktop\RMZREPO\Game2014\proj.android\../cocos2d/cocos/2d/ccFPSImages.c \
- C:\Users\Ricardo\Desktop\RMZREPO\Game2014\proj.android\../cocos2d/cocos/2d/ccFPSImages.h
-
-C:\Users\Ricardo\Desktop\RMZREPO\Game2014\proj.android\../cocos2d/cocos/2d/ccFPSImages.h:
