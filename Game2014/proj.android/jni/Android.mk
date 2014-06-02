80822de8c935b7670a3bb2aa2266028ab0c380a9
Mark Meritt <m.meritt93@hotmail.com>
2014-06-01 20:56:08 -0400

    no message

--- Game2014/proj.android/jni/Android.mk
+++ Game2014/proj.android/jni/Android.mk
@@ -7,20 +7,29 @@ LOCAL_MODULE := cocos2dcpp_shared
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp\
-			   ../../Classes/Player.cpp\
-			   ../../Classes/Balloon.cpp\
-			   ../../Classes/Background.cpp\
-			   ../../Classes/PullIndicator.cpp\
-			   ../../Classes/MainMenu.cpp\
-			   ../../Classes/Civilian.cpp\
-			   ../../Classes/RMZHelper.cpp\
-			   ../../Classes/Options.cpp\
-			   ../../Classes/CivFactory.cpp\
-			   ../../Classes/RMZScene.cpp\
-			   ../../Classes/ArrowBtn.cpp\
+            ../../Classes/AppDelegate.cpp\
+                   	../../Classes/HelloWorldScene.cpp\
+			../../Classes/Civilian.cpp\
+			../../Classes/RMZHelper.cpp\
+			../../Classes/Options.cpp\
+			../../Classes/CivFactory.cpp\
+			../../Classes/RMZScene.cpp\
+			../../Classes/ArrowBtn.cpp\
 			../../Classes/SceneManager.cpp\
+			../../Classes/MainMenu.cpp\
+			../../Classes/Background.cpp\
+			../../Classes/Audio.cpp\
+			../../Classes/Balloon.cpp\
+			../../Classes/Credits.cpp\
+			../../Classes/GameOver.cpp\
+			../../Classes/HighScore.cpp\
+			../../Classes/HowToPlay.cpp\
+			../../Classes/LoadingScreen.cpp\
+			../../Classes/Player.cpp\
+			../../Classes/PullIndicator.cpp\
+			../../Classes/Social.cpp\
+			../../Classes/Splash.cpp\
+
 
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
